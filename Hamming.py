5b45c518dc0414b987296d616008aeba6a42b82b
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-09-23 17:18:10 -0500

    ok

--- Hamming.py
+++ Hamming.py
@@ -23,9 +23,9 @@ def encode(p):
   x = x & 1
   return x
 
-# def makeError(p): 
-#   rdm=random.randint(0,p.shape[0]-1)
-#   p[rdm]=p[rdm]^1;
+def makeError(p): 
+  rdm=random.randint(0,p.shape[0]-1)
+  p[rdm]=p[rdm]^1;
 
 def parityCheck(r): 
   H74 = np.array([ \
@@ -45,56 +45,44 @@ def decodeMessage(r):
   pr=np.matmul(R74,r)
   return pr
 
-"""
-Used for debugging
-"""
-def debug():
-  p=makeMessage(4)
-  x=encode(p)
-  z=parityCheck(x) 
-  pr=decodeMessage(x)
-
-debug()
-
-
 
 """
   p - the message vector
   pLen - length of p, message
   returns nothing
 """
-# def main():
-#   # enter mode: either (7,4) or (15,11)
-#   mode=input("Enter mode: ")
-#   if mode=="H1511": pLen=11
-#   else : pLen=4
-# 
-#   # generate random message vector, p, of length 4 or 11 
-#   p = makeMessage();
-#   print("Message: "+message)
-# 
-#   # encode (make send vector)
-#   sendVector=encode(message,pLen)
-#   print("Send Vector: "+sendVector)
-# 
-#   # modify the vector to simulate an error or not
-#   recievedMessage=makeError()
-#   print("Recieved Message: "+recievedMessage)
-# 
-#   # Parity Check
-#   z=parityCheck()
-#   print("Parity Check: "+z);
-# 
-#   # Error Correction
-#   correctError()
-#   print("Corrected Message: ")
-# 
-#   # Decode Message 
-#   pr=decodeMesssage()
-#   print("Decoded Message: "+pr);
-#   
-# 
-#
+def main():
+  # enter mode: either (7,4) or (15,11)
+  mode=input("Enter mode: ")
+  if mode=="H1511": pLen=11
+  else : pLen=4
+
+  # generate random message vector, p, of length 4 or 11 
+  p = makeMessage();
+  print("Message: "+message)
+
+  # encode (make send vector)
+  sendVector=encode(message,pLen)
+  print("Send Vector: "+sendVector)
+
+  # modify the vector to simulate an error or not
+  recievedMessage=makeError()
+  print("Recieved Message: "+recievedMessage)
+
+  # Parity Check
+  z=parityCheck()
+  print("Parity Check: "+z);
+
+  # Error Correction
+  correctError()
+  print("Corrected Message: ")
+
+  # Decode Message 
+  pr=decodeMesssage()
+  print("Decoded Message: "+pr);
+  
+
+
 
 
 
