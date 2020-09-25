d959d0081119b05b1fb2d37ef26e0adc065bb7de
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-09-25 01:13:28 -0500

    back to (7,4)

--- Hamming.py
+++ Hamming.py
@@ -8,66 +8,6 @@ import numpy as np
 import random 
 import math
 
-
-def encodeN(p):
-  n=p.shape[0]
-  parityBits=int(math.log(n,2))+1
-  print("parityBits: "+str(parityBits))
-  print("n: "+str(n))
-
-  I = np.identity(n).astype(int)
-  print("A:")
-  A = np.flip((np.identity(n).astype(int)^1)[:parityBits][:],0)
-  print(A)
-
-  G = np.concatenate((A,I), axis=0)
-  print("Gn: ")
-  print(G)
-
-encodeN(np.array([[1,0,1,1,0]]).transpose())
-exit()
-
-def parityCheckN(r):
-  I = np.identity(n).astype(int)
-  A = np.fliplr((np.identity(n).astype(int)^1)[n-parityBits:][:])
-
-
-
-
-
-
-def encode3(p):
-  G74=np.array([ \
-    [1,1,0,1], \
-    [1,0,1,1], \
-    [0,1,1,1], \
-    [1,0,0,0], \
-    [0,1,0,0], \
-    [0,0,1,0], \
-    [0,0,0,1]])
-  x = np.matmul(G74,p)
-  x = x & 1
-  return x
-
-def parityCheck3(r): 
-  H74 = np.array([ \
-    [1,0,0,1,1,0,1], \
-    [0,1,0,1,0,1,1], \
-    [0,0,1,0,1,1,1]])
-  z = np.matmul(H74,r)
-  z = z & 1
-  return z
-
-def decodeMessage3(r): 
-  R74 = np.array([ \
-    [0,0,0,1,0,0,0], \
-    [0,0,0,0,1,0,0], \
-    [0,0,0,0,0,1,0], \
-    [0,0,0,0,0,0,1]])
-  pr=np.matmul(R74,r)
-  return pr
-
-#-----------------------------------------------------------
 def makeMessage(n): 
   return np.random.randint(2, size=(n, 1))
 
@@ -115,7 +55,7 @@ def correctError(z,r):
   r[loc-1,0]=r[loc-1,0]^1;
   return r
 
-
+#-----------------------------------------------------------
 def main():
   # enter mode: either (7,4) or (15,11)
   mode=input("Enter mode: ")
@@ -127,7 +67,7 @@ def main():
   print("Message:           "+str(p.transpose()))
 
   # encode (make send vector)
-  x=encode3(p)
+  x=encode(p)
   print("Send Vector:       "+str(x.transpose()))
 
   # modify the vector to simulate an error or not
@@ -135,7 +75,7 @@ def main():
   print("Recieved Message:  "+str(r.transpose()))
 
   # Parity Check
-  z=parityCheck3(r)
+  z=parityCheck(r)
   print("Parity Check:      "+str(z.transpose()));
 
   # Error Correction
@@ -143,13 +83,9 @@ def main():
   print("Corrected Message: "+str(corrected.transpose()))
 
   # Decode Message 
-  pr=decodeMessage3(x)
+  pr=decodeMessage(x)
   print("Decoded Message:   "+str(pr.transpose()));
   
-#main()
-
-
-
-
+main()
 
 
